53907c89b0fbb0cc4516cb2c17c171e98ebbfcbd
kokobd <contact@zelinf.net>
2022-06-17 16:39:25 +0800

    split user ssh config

--- kokobd/default.nix
+++ kokobd/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  imports = [ ./openvscode-server.nix ];
+  imports = [ ./openvscode-server.nix ./ssh.nix ];
 
   home.packages = with pkgs; [ neovide ];
   programs = {
@@ -12,35 +12,6 @@
       userEmail = "contact@zelinf.net";
       ignores = [ "*.swp" ".vscode/" ".idea/" ];
       extraConfig = { core = { editor = "vim"; }; };
-    };
-    ssh = {
-      enable = true;
-      serverAliveInterval = 10;
-      serverAliveCountMax = 10;
-      matchBlocks = let identityFile = "/home/kokobd/.ssh/id_rsa";
-      in {
-        "github.com" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = "/home/kokobd/.ssh/id_ed25519";
-        };
-        tencent = {
-          hostname = "119.91.200.28";
-          user = "ubuntu";
-          inherit identityFile;
-        };
-        home = {
-          hostname = "119.91.200.28";
-          user = "kokobd";
-          port = 10022;
-          inherit identityFile;
-        };
-        home-gateway = {
-          hostname = "192.168.31.2";
-          user = "pi";
-          inherit identityFile;
-        };
-      };
     };
   };
 
---
+++ kokobd/ssh.nix
@@ -0,0 +1,37 @@
+{ ... }:
+
+{
+  programs = {
+    ssh = {
+      enable = true;
+      serverAliveInterval = 10;
+      serverAliveCountMax = 10;
+      matchBlocks =
+        let identityFile = "/home/kokobd/.ssh/id_rsa";
+        in
+        {
+          "github.com" = {
+            hostname = "github.com";
+            user = "git";
+            identityFile = "/home/kokobd/.ssh/id_ed25519";
+          };
+          tencent = {
+            hostname = "119.91.200.28";
+            user = "ubuntu";
+            inherit identityFile;
+          };
+          home = {
+            hostname = "119.91.200.28";
+            user = "kokobd";
+            port = 10022;
+            inherit identityFile;
+          };
+          home-gateway = {
+            hostname = "192.168.31.2";
+            user = "pi";
+            inherit identityFile;
+          };
+        };
+    };
+  };
+}
