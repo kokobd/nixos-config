3f5910ac5823b4a643e0933a474df1f90a505a14
kokobd <contact@zelinf.net>
2022-06-19 10:21:44 +0800

    disable DHCP

--- networking.nix
+++ networking.nix
@@ -6,10 +6,14 @@
     networkmanager.enable = true;
     firewall.enable = false;
 
-    interfaces.enp89s0.ipv4.addresses = [{
-      address = "192.168.31.3";
-      prefixLength = 16;
-    }];
+    interfaces.enp89s0 = {
+      useDHCP = false;
+
+      ipv4.addresses = [{
+        address = "192.168.31.3";
+        prefixLength = 16;
+      }];
+    };
 
     defaultGateway = "192.168.31.2";
     nameservers = [ "8.8.8.8" ];
