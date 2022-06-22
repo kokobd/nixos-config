bc60aa192fc2870ab00ff6b4bd24539f0694ba94
kokobd <contact@zelinf.net>
2022-06-22 11:05:38 +0800

    split fhs envs by ghc version

--- kokobd/fhs.nix
+++ kokobd/fhs.nix
@@ -1,27 +1,39 @@
-{ config, pkgs, ... }: {
-  programs.bash.shellAliases = {
-    fhs = "nix-shell ${config.home.homeDirectory}/.fhs.nix";
+{ config, pkgs, ... }:
+let
+  compilers = [ "ghc884" "ghc8107" "ghc902" "ghc923" ];
+  mkShellAlias = compiler: {
+    name = "fhs-${compiler}";
+    value = "nix-shell ${config.home.homeDirectory}/.fhs-${compiler}.nix";
   };
 
-  home.file.".fhs.nix".text = ''
-    { pkgs ? import <nixpkgs> { } }:
-    (pkgs.buildFHSUserEnv {
-      name = "global";
-      targetPkgs = pkgs: with pkgs; [
-        coreutils
-        gcc
-        zlib.dev
-        gmp.dev
-        python
-        haskell.compiler.ghc923
-        haskell.compiler.ghc902
-        haskell.compiler.ghc8107
-        cabal-install
-        stack
-        haskellPackages.hoogle
-        stylish-haskell
-        pre-commit
-      ];
-    }).env
-  '';
+  buildHaskellFHS = compiler: {
+    name = ".fhs-${compiler}.nix";
+    value = {
+      text = ''
+        { pkgs ? import <nixpkgs> { } }:
+        (pkgs.buildFHSUserEnv {
+          name = "global";
+          targetPkgs = pkgs: with pkgs; [
+            coreutils
+            gcc
+            zlib.dev
+            gmp.dev
+            python
+            haskell.compiler.${compiler}
+            cabal-install
+            stack
+            haskellPackages.hoogle
+            haskellPackages.implicit-hie
+            stylish-haskell
+            pre-commit
+          ];
+        }).env
+      '';
+    };
+  };
+in {
+  programs.bash.shellAliases =
+    builtins.listToAttrs (map mkShellAlias compilers);
+
+  home.file = builtins.listToAttrs (map buildHaskellFHS compilers);
 }
